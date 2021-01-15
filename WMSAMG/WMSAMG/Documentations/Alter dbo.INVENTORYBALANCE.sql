016daa32ab9115256d5f8d66fdbea82d08221d56
Kevin Llupar <nivekrapull143@gmail.com>
2021-01-15 15:29:07 +0800

    changes 01112021

---
+++ WMSAMG/WMSAMG/Documentations/Alter dbo.INVENTORYBALANCE.sql
@@ -0,0 +1,37 @@
+﻿USE [CSIS2017]
+GO
+
+/****** Object: Table Valued Function [dbo].[INVENTORYBALANCE] Script Date: 01/09/2021 1:45:55 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+ALTER FUNCTION [dbo].[INVENTORYBALANCE]
+(	
+	@CustomerID	as varchar(100),
+	@StockID as uniqueidentifier,
+	@PalletNo as varchar(100),
+	@ProductionDate as date
+)
+RETURNS TABLE 
+AS
+RETURN 
+(
+	SELECT TOP 1 SUM(m1.Qty * m1.Signed) Qty,
+	SUM(m1.ActualWeight * m1.Signed) ActualWeight,
+	MAX(m1.ProductionDate) ProductionDate
+	  FROM vw_MasterTransactions m1 
+	  WHERE m1.CustomerID=@CustomerID 
+	  AND m1.StockID=@StockID
+	  AND m1.PalletNo=@PalletNo
+	  AND m1.ProductionDate<=@ProductionDate AND m1.isSaved=1
+	GROUP BY ReferenceNo,StockID,StockPcsperPack
+)
---
+++ WMSAMG/WMSAMG/Documentations/Alter dbo.vw_MasterTransactions.sql
@@ -0,0 +1,65 @@
+﻿USE [CSIS2017]
+GO
+
+/****** Object: View [dbo].[vw_MasterTransactions] Script Date: 01/09/2021 1:46:12 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+
+
+
+
+
+
+
+ALTER VIEW [dbo].[vw_MasterTransactions]
+
+AS
+SELECT	 m.RefCode
+      ,m.ReferenceNo
+      ,m.CustomerID
+	  ,cu.CustomerName
+      ,m.PayTypeInitial
+      ,m.StockID
+	  ,st.StockDescription
+      ,m.StockSKU
+      ,m.StockGroupID
+      ,m.StockPcsperPack
+      --,m.StockPackperCase
+      ,m.Qty
+      ,m.ActualWeight
+      ,m.UOM
+	  ,n.Signed
+      ,m.ProductionDate
+      --,m.StockWeightinKilosperPack
+      --,m.StockWeightinKilosperCase
+      ,m.PalletNo
+      ,m.CompanyID
+      ,m.StorageLocationID
+	  ,s.StorageLocationName
+	  ,s.StorageName
+      ,m.StorageID
+      ,m.StorageTypeID
+      ,m.TransactionDate
+      ,m.LocationID
+      ,m.Nature
+      ,m.Source
+      ,m.Remarks
+      ,m.ApprovedBy
+      ,m.EmployeeID
+      ,m.EmployeeDate
+	  ,c.CompanyInitial
+	  ,l.LocationInitial
+      ,m.isSaved
+FROM tblMasterTransaction m
+OUTER APPLY NATURE_FIRSTONLY(m.Nature) n
+OUTER APPLY STORAGELOCATION_FIRSTONLY(m.StorageLocationID) s
+LEFT OUTER JOIN CSISControl.dbo.tblCompany c ON m.CompanyID=c.CompanyID
+LEFT OUTER JOIN CSISControl.dbo.vw_StocktoStockGrouptoCustomerandCompany st ON m.StockID = st.StockID 
+LEFT OUTER JOIN CSISControl.dbo.vw_Location l ON m.LocationID=l.LocationID 
+LEFT OUTER JOIN CSISControl.dbo.vw_CustomertoCompanyandLocation cu ON m.CustomerID=cu.CustomerID
